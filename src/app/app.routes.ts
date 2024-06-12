08e62569a3c9717beef2609f97af4178719dff4b
Alvaro <alvaro.ledesmav@gmail.com>
2024-06-12 16:16:10 +0200

    Routes set up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./dashboard/dashboard.component'),
+        children: [
+            {
+                path: 'general-taks',
+                title: 'General Tasks',
+                loadComponent: () => import('./dashboard/pages/general-tasks/general-tasks.component'),
+            },
+            {
+                path: 'regulatory-tasks',
+                title: 'Regulatory Tasks',
+                loadComponent: () => import('./dashboard/pages/regulatory-tasks/regulatory-tasks.component'),
+            },
+
+            {
+                path: '', redirectTo: 'general-taks', pathMatch: 'full',
+            }
+        ]
+    },{
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full',
+    }
+
+
+];
